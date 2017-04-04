f192114f98f6d299e2ea5585f5c0d411b864cec6
oshibori <oshibori0121vox@gmail.com>
2017-04-05 04:42:18 +0900

    ARC-A

---
+++ AtCoder/AtCoder/arc49a.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+#ifdef _DEBUG
+#include "dump.hpp"
+#else
+#define dump(...)
+#endif
+
+//#define int long long
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define rrep(i,a,b) for(int i=(b)-1;i>=(a);i--)
+#define all(c) begin(c),end(c)
+const int INF = sizeof(int) == sizeof(long long) ? 0x3f3f3f3f3f3f3f3fLL : 0x3f3f3f3f;
+const int MOD = (int)(1e9) + 7;
+const double PI = acos(-1);
+const double EPS = 1e-9;
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+
+signed main() {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	string s; cin >> s;
+	vector<int>v(4); rep(i, 0, 4)cin >> v[i];
+	rep(i, 0, 4) {
+		v[i] += i;
+		dump(v[i]);
+		s.insert(v[i], "\"");
+		dump(s);
+	}
+	cout << s << endl;
+	return 0;
+}
